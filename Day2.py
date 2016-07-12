3651482fb26a428cc43f0b14e776107cf4dc8182
kobed2241 <kobed2241@afsenyc.org>
2016-07-12 11:44:37 -0400

    Day2
    Adjustable size and speed of square

---
+++ Day2.py
@@ -0,0 +1,12 @@
+from Myro import * #x is speed (x,y)
+init("sim") 
+size = 1
+speed = 1
+forward(speed,size)
+turnBy(90)
+forward(speed,size)
+turnBy(90)
+forward(speed,size)
+turnBy(90)
+forward(speed,size)
+turnBy(90)
